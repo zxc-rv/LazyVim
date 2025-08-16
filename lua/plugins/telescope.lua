b9795d00066b9ac13c2d7d71edba00afbc5f7904
zxc-rv <the.revolution@icloud.com>
2025-08-16 19:11:19 +0300

    config update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
   config = function()
     require("telescope").setup({
       defaults = {
@@ -12,6 +16,15 @@ return {
           hide_on_startup = true,
         },
       },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+      extensions = {
+        fzf = {},
+      },
     })
+    require("telescope").load_extension("fzf")
   end,
 }
